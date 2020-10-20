6ed9fdf672b02cf1fd22f8563bbe1f49c6245a7f
biguokang <biguokang@vtron.com>
2020-10-20 12:47:05 +0800

    update demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,4 +1,4 @@
-import qrcode from '../mod.js'
+import qrcode from 'https://deno.land/x/qrcode_terminal/mod.js'
 
 console.log("\n\nScan It Skip To Google 👇\n")
 qrcode.generate("https://www.google.com")
