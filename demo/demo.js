eb3e8a3cb8407a6a2786e0e0a246289bd7fdf700
biguokang <biguokang@vtron.com>
2020-10-20 12:25:12 +0800

    first commit

---
+++ demo/demo.js
@@ -0,0 +1,4 @@
+import qrcode from '../mod.js'
+
+console.log("\n\nScan It Skip To Google 👇\n")
+qrcode.generate("https://www.google.com")
---
+++ example/test.js
@@ -0,0 +1,2 @@
+import qrcode from '../lib/main.js'
+qrcode.generate('https://u.wechat.com/MI8g1d4fSdEntqOdCrp-DU8', {small:true})
